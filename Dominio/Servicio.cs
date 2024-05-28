7182c63d62d171108b97cc1c9c12d1858953698b
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-05-28 20:52:13 -0300

    Se agrega clase Servicio a Dominio

---
+++ Dominio/Servicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Servicio
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set;}
+        public int Duracion { get; set; }
+        public float Precio { get; set; }
+    }
+}
