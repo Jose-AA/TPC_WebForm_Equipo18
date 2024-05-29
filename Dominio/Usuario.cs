64360c76d1e485589938f6d9ca858efdc9d4520f
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-05-28 21:06:44 -0300

    Primera version de la clase Usuario

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+
+    /*
+     ROLES:
+        - Cliente
+        - Especialista
+        - Administrador
+
+    EN TODOS LOS CASOS, SERAN USUARIOS REGISTRADOS EN LA APLICACION WEB
+     */
+
+
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public int IdRol { get; set; }
+    }
+}
