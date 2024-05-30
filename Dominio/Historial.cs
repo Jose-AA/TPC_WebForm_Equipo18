a808c1b3af7bec9478c055a5ef5672c1bd8c4e0c
MartinAgarcia1710 <118487586+MartinAgarcia1710@users.noreply.github.com>
2024-05-30 20:37:23 -0300

    Clase historial
    Agregué la clase historial que me había olvidado. En la misma, a modo de comentario enumero 3 opciones para que se efectúen los historiales de lo que se le hace en cada turno a los clientes.

---
+++ Dominio/Historial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    
+
+namespace Dominio
+{
+    public class Historial
+    {
+        /*
+        Opción 1:
+            public Cliente Cliente { get; set; }
+            public Especialista Especialista { get; set; }
+            public Servicio Servicio { get; set; }
+        
+        Opción 2:
+            public Turno Turno { get; set; }
+
+        Opción 3:
+            Agregar la propiedad observaciones a la clase Turno ya que todo turno que se
+            concrete va a tener historial. Además cada cliente tiene su lista de turnos.
+        */
+        public string Observaciones { get; set; }
+
+    }
+}
