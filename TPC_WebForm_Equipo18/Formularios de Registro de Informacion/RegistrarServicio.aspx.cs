07b7dbc8e8d5d14f1762ec3145ba1e5506606889
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-06-02 13:06:31 -0300

    Validaciones en la carga de servicios

--- TPC_WebForm_Equipo18/Formularios de Registro de Informacion/RegistrarServicio.aspx.cs
+++ TPC_WebForm_Equipo18/Formularios de Registro de Informacion/RegistrarServicio.aspx.cs
@@ -18,11 +18,23 @@ namespace TPC_WebForm_Equipo18.Formularios_de_Registro_de_Informacion
 
         protected void btnCrearServicio_Click(object sender, EventArgs e)
         {
+
+            //Validamos, y si algo da error terminamos la ejecucion del evento
+            Page.Validate();
+            if (!Page.IsValid)
+                return;
+
+            if(String.IsNullOrEmpty(inputNombre.Text) || String.IsNullOrEmpty(inputDescripcion.Text) 
+                || String.IsNullOrEmpty(inputDuracion.Text) || String.IsNullOrEmpty(inputPrecio.Text))
+            {
+                return;
+            }
+
             // Obtener los datos ingresados por el usuario
-            string nombre = inputNombre.Text;
-            string descripcion = inputDescripcion.Text;
-            int duracion = Convert.ToInt32(inputDuracion.Text);
-            float precio = Convert.ToSingle(inputPrecio.Text);
+            string nombre = inputNombre.Text.Trim();
+            string descripcion = inputDescripcion.Text.Trim();
+            int duracion = Convert.ToInt32(inputDuracion.Text.Trim());
+            float precio = Convert.ToSingle(inputPrecio.Text.Trim());
 
             // Crear un objeto Servicio con los datos ingresados
             Servicio nuevoServicio = new Servicio
@@ -41,6 +53,12 @@ namespace TPC_WebForm_Equipo18.Formularios_de_Registro_de_Informacion
 
                 // Llamar a la función para abrir la modal de éxito
                 ScriptManager.RegisterStartupScript(this, this.GetType(), "modalServicioAgregado", "abrirModalServicioAgregado();", true);
+
+                //Limpiamos las cajas de texto para la siguiente carga de datos:
+                inputNombre.Text = "";
+                inputDescripcion.Text = "";
+                inputDuracion.Text = "";
+                inputPrecio.Text = "";
             }
             catch (Exception ex)
             {
