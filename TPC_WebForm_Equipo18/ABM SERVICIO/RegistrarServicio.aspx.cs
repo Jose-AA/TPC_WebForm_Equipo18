f657c4d332a5ac6d02e18956d1e64015156661b7
Jose Arias <jose.arias@alumnos.frgp.utn.edu.ar>
2024-06-14 23:45:48 -0300

    ABM COMPLETO DE SERVICIO

---
+++ TPC_WebForm_Equipo18/ABM SERVICIO/RegistrarServicio.aspx.cs
@@ -0,0 +1,226 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_WebForm_Equipo18.Formularios_de_Registro_de_Informacion
+{
+    public partial class RegistrarServicio : System.Web.UI.Page
+    {
+
+        List<Servicio> listaServicios = new List<Servicio>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ServicioNegocio negocio = new ServicioNegocio();
+                listaServicios = negocio.listar();
+
+                ModificarDropDownList.DataSource = listaServicios;
+                ModificarDropDownList.DataTextField = "Nombre";
+                ModificarDropDownList.DataValueField = "Id";
+                ModificarDropDownList.DataBind();
+                ModificarDropDownList.Items.Insert(0, new ListItem("Seleccione un servicio", "0"));
+
+                EliminarDropDownList.DataSource = listaServicios;
+                EliminarDropDownList.DataTextField = "Nombre";
+                EliminarDropDownList.DataValueField = "Id";
+                EliminarDropDownList.DataBind();
+                EliminarDropDownList.Items.Insert(0, new ListItem("Seleccione un servicio", "0"));
+            }
+        }
+
+        protected void btnCrearServicio_Click(object sender, EventArgs e)
+        {
+
+            //Validamos, y si algo da error terminamos la ejecucion del evento
+            Page.Validate();
+            if (!Page.IsValid)
+                return;
+
+            if (String.IsNullOrEmpty(inputNombre.Text) || String.IsNullOrEmpty(inputDescripcion.Text)
+                || String.IsNullOrEmpty(inputDuracion.Text) || String.IsNullOrEmpty(inputPrecio.Text))
+            {
+                return;
+            }
+
+            if (inputDescripcion.Text.Trim().Length > 100 || inputNombre.Text.Trim().Length > 50)
+            {
+                return;
+            }
+
+           
+            string nombre = inputNombre.Text.Trim();
+            string descripcion = inputDescripcion.Text.Trim();
+            int duracion = Convert.ToInt32(inputDuracion.Text.Trim());
+            decimal precio = Convert.ToDecimal(inputPrecio.Text.Trim());
+
+           
+            Servicio nuevoServicio = new Servicio
+            {
+                Nombre = nombre,
+                Descripcion = descripcion,
+                Duracion = duracion,
+                Precio = precio
+            };
+
+            try
+            {
+             
+                ServicioNegocio negocio = new ServicioNegocio();
+                negocio.Agregar(nuevoServicio);
+
+              
+                List<Servicio> aux = (List<Servicio>)Session["ListaServicios"];
+                aux.Add(nuevoServicio);
+                Session["ListaServicios"] = aux;
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "modalServicioAgregado", "abrirModalServicioAgregado();", true);
+
+                //Limpiamos las cajas de texto para la siguiente carga de datos:
+                inputNombre.Text = "";
+                inputDescripcion.Text = "";
+                inputDuracion.Text = "";
+                inputPrecio.Text = "";
+
+                //Aunque tal vez mejor que limpiar, sería hacer un redirect al panel de administrador...
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error al agregar el servicio: " + ex.Message);
+            }
+        }
+
+
+        protected void ModificarDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idServicio = int.Parse(ModificarDropDownList.SelectedValue);
+            List<Servicio> listaServicios = new List<Servicio>();
+            ServicioNegocio negocio = new ServicioNegocio();
+            listaServicios = negocio.listar();
+            if (idServicio != 0)
+            {
+                Servicio servicio = listaServicios.FirstOrDefault(s => s.Id == idServicio);
+                if (servicio != null)
+                {
+                    inputNombreModificar.Text = servicio.Nombre;
+                    inputDescripcionModificar.Text = servicio.Descripcion;
+                    inputDuracionModificar.Text = servicio.Duracion.ToString();
+                    // Convertir el precio a string y reemplazar la coma por el punto
+                    string precio = servicio.Precio.ToString().Replace(',', '.');
+
+                    // Asignar el precio al TextBox
+                    inputPrecioModificar.Text = precio;
+
+                }
+            }
+        }
+
+        protected void EliminarDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idServicio = int.Parse(EliminarDropDownList.SelectedValue);
+            List<Servicio> listaServicios = new List<Servicio>();
+            ServicioNegocio negocio = new ServicioNegocio();
+            listaServicios = negocio.listar();
+            if (idServicio != 0)
+            {
+                Servicio servicio = listaServicios.FirstOrDefault(s => s.Id == idServicio);
+                if (servicio != null)
+                {
+                    inputNombreEliminar.Text = servicio.Nombre;
+                    inputDescripcionEliminar.Text = servicio.Descripcion;
+                    inputDuracionEliminar.Text = servicio.Duracion.ToString();
+
+                    // Convertir el precio a string y reemplazar la coma por el punto
+                    string precio = servicio.Precio.ToString().Replace(',', '.');
+
+                    // Asignar el precio al TextBox
+                    inputPrecioEliminar.Text = precio;
+                }
+            }
+        }
+
+        protected void btnModificarServicio_Click(object sender, EventArgs e)
+        {
+            int idServicio = int.Parse(ModificarDropDownList.SelectedValue);
+            List<Servicio> listaServicios = new List<Servicio>();
+            ServicioNegocio negocio = new ServicioNegocio();
+            listaServicios = negocio.listar();
+
+            if (idServicio != 0)
+            {
+                // Obtener el servicio de la lista por su ID
+                Servicio servicio = listaServicios.FirstOrDefault(s => s.Id == idServicio);
+                if (servicio != null)
+                {
+                    // Actualizar los campos del objeto Servicio con los valores de los TextBox
+                    servicio.Nombre = inputNombreModificar.Text;
+                    servicio.Descripcion = inputDescripcionModificar.Text;
+                    servicio.Duracion = int.Parse(inputDuracionModificar.Text);
+                    string precio = inputPrecioModificar.Text.Replace('.', ',');
+                    servicio.Precio = decimal.Parse(precio); // Asegúrate de convertir a decimal si corresponde
+
+                    try
+                    {
+
+
+                        negocio.Modificar(servicio);
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "modalServicioAgregado", "modificadoexitosamente()", true);
+                      
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString(); // Manejar la excepción
+                    }
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "modalServicioAgregado", "seleccione()", true);
+            }
+        }
+
+        protected void btnEliminarServicio_Click(object sender, EventArgs e)
+        {
+            int idServicio = int.Parse(EliminarDropDownList.SelectedValue);
+            ServicioNegocio negocio = new ServicioNegocio();
+
+            if (idServicio != 0)
+            {
+                try
+                {
+                    // Llamar al método eliminar del negocio pasando el idServicio
+                    negocio.Eliminar(idServicio);
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "modalServicioAgregado", "Eliminadoexitosamente()", true);
+                    
+                    
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    // Manejar la excepción si ocurre algún error
+                  
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "modalServicioAgregado", "seleccione()", true);
+            }
+        }
+
+    }
+
+
+
+
+
+}
+
+
---
+++ TPC_WebForm_Equipo18/ABM SERVICIO/RegistrarServicio.aspx.designer.cs
@@ -0,0 +1,251 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_WebForm_Equipo18.Formularios_de_Registro_de_Informacion
+{
+
+
+    public partial class RegistrarServicio
+    {
+
+        /// <summary>
+        /// Control ScriptManager1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// Control UpdatePanelCrearServicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanelCrearServicio;
+
+        /// <summary>
+        /// Control inputNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputNombre;
+
+        /// <summary>
+        /// Control rfvNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvNombre;
+
+        /// <summary>
+        /// Control inputDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputDescripcion;
+
+        /// <summary>
+        /// Control rfvDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvDescripcion;
+
+        /// <summary>
+        /// Control inputDuracion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputDuracion;
+
+        /// <summary>
+        /// Control rfvDuracion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvDuracion;
+
+        /// <summary>
+        /// Control rvDuracion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator rvDuracion;
+
+        /// <summary>
+        /// Control inputPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputPrecio;
+
+        /// <summary>
+        /// Control rfvPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPrecio;
+
+        /// <summary>
+        /// Control rvPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator rvPrecio;
+
+        /// <summary>
+        /// Control btnCrearServicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCrearServicio;
+
+        /// <summary>
+        /// Control Table1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
+
+        /// <summary>
+        /// Control ModificarDropDownList.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ModificarDropDownList;
+
+        /// <summary>
+        /// Control inputNombreModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputNombreModificar;
+
+        /// <summary>
+        /// Control inputDescripcionModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputDescripcionModificar;
+
+        /// <summary>
+        /// Control inputDuracionModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputDuracionModificar;
+
+        /// <summary>
+        /// Control inputPrecioModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputPrecioModificar;
+
+        /// <summary>
+        /// Control btnModificarServicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificarServicio;
+
+        /// <summary>
+        /// Control EliminarDropDownList.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList EliminarDropDownList;
+
+        /// <summary>
+        /// Control inputNombreEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputNombreEliminar;
+
+        /// <summary>
+        /// Control inputDescripcionEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputDescripcionEliminar;
+
+        /// <summary>
+        /// Control inputDuracionEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputDuracionEliminar;
+
+        /// <summary>
+        /// Control inputPrecioEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputPrecioEliminar;
+
+        /// <summary>
+        /// Control btnEliminarServicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarServicio;
+    }
+}
