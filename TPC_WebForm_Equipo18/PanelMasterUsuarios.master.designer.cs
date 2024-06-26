4127006aca8755dcfd557bcb0d10960adba46887
MartinAgarcia1710 <118487586+MartinAgarcia1710@users.noreply.github.com>
2024-06-25 23:29:27 -0300

    Master
    Se creo master para el panel de los usuarios. con offcanva y dropdown lists

---
+++ TPC_WebForm_Equipo18/PanelMasterUsuarios.master.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_WebForm_Equipo18
+{
+
+
+    public partial class PanelMasterUsuarios
+    {
+    }
+}
---
+++ TPC_WebForm_Equipo18/Recepción/RecepcionIngreso.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PanelMasterUsuarios.master" AutoEventWireup="true" CodeBehind="RecepcionIngreso.aspx.cs" Inherits="TPC_WebForm_Equipo18.Recepción.RecepcionIngreso" %>
